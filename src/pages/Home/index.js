c5ee1617ebd2beb1e0e59b2fa93518034b773807
Denner Torres <dennerstorres@icloud.com>
2021-02-11 12:56:23 -0400

    Initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { GeneralText } from './styles';
+
+function Home() {
+  return (
+    <GeneralText>
+      Hellowwww
+    </GeneralText>
+  );
+}
+
+export default Home;
