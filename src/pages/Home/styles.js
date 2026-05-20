9631cf34ec86f3305f7f0327d017501527fa93ba
dennerstorres <48073900+dennerstorres@users.noreply.github.com>
2026-05-20 18:31:43 +0000

    feat: implement empty state for task list

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -46,7 +46,20 @@ export const ButtonAdd = styled.TouchableOpacity`
   align-items: center;
 `;
 
-export const TaskList = styled.View`
+export const TaskList = styled.FlatList`
   width: 100%;
   margin-top: 10px;
+`;
+
+export const EmptyStateContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+export const EmptyStateText = styled.Text`
+  color: #9cc5a1;
+  font-size: 16px;
+  margin-top: 10px;
+  text-align: center;
 `;
