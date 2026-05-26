f6540c7848633ec398afc44911ed88b251346d7a
dennerstorres <48073900+dennerstorres@users.noreply.github.com>
2026-05-26 18:14:52 +0000

    refactor: extract shared styled components for inputs and buttons
    - Created src/components/Shared/styles.js with BaseInputContainer, BaseClearButton, and BaseInput.
    - Refactored AddTask and Search components to extend these shared styles.
    - Updated Jest snapshots to reflect style normalization.
    - Updated ROADMAP.md documentation.

--- src/components/AddTask/styles.js
+++ src/components/AddTask/styles.js
@@ -1,35 +1,23 @@
 import styled from 'styled-components/native';
+import {BaseInputContainer, BaseClearButton, BaseInput} from '../Shared/styles';
 
 export const TaskAdd = styled.View`
   flex-direction: row;
   margin-top: ${props => props.theme.spacing.medium};
 `;
 
-export const InputContainer = styled.View`
+export const InputContainer = styled(BaseInputContainer)`
   flex: 1;
-  flex-direction: row;
-  align-items: center;
-  background-color: ${props => props.theme.colors.white};
-  height: 40px;
-  border-width: 1px;
-  border-color: ${props => props.theme.colors.accent};
 `;
 
-export const TaskText = styled.TextInput.attrs(props => ({
-  placeholderTextColor: props.theme.colors.accent,
-}))`
-  flex: 1;
+export const TaskText = styled(BaseInput)`
   height: 40px;
   padding-left: ${props => props.theme.spacing.medium};
-  color: ${props => props.theme.colors.text};
-  font-size: ${props => props.theme.typography.size.medium};
-  font-weight: ${props => props.theme.typography.weight.regular};
 `;
 
-export const ClearButton = styled.TouchableOpacity`
+export const ClearButton = styled(BaseClearButton)`
   padding: 0 ${props => props.theme.spacing.small};
   height: 100%;
-  justify-content: center;
 `;
 
 export const ButtonAdd = styled.TouchableOpacity`
--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,30 +1,20 @@
 import styled from 'styled-components/native';
+import {BaseInputContainer, BaseClearButton, BaseInput} from '../Shared/styles';
 
 export const SearchContainer = styled.View`
   padding: ${props => props.theme.spacing.small}
     ${props => props.theme.spacing.large};
 `;
 
-export const InputContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  background-color: ${props => props.theme.colors.white};
+export const InputContainer = styled(BaseInputContainer)`
   border-radius: ${props => props.theme.spacing.small};
   padding: 0 ${props => props.theme.spacing.medium};
-  border: 1px solid ${props => props.theme.colors.accent};
-  height: 40px;
 `;
 
-export const SearchInput = styled.TextInput.attrs(props => ({
-  placeholderTextColor: props.theme.colors.accent,
-}))`
-  flex: 1;
-  color: ${props => props.theme.colors.text};
-  font-weight: ${props => props.theme.typography.weight.regular};
-  font-size: ${props => props.theme.typography.size.medium};
+export const SearchInput = styled(BaseInput)`
   padding: 0 ${props => props.theme.spacing.small};
 `;
 
-export const ClearButton = styled.TouchableOpacity`
+export const ClearButton = styled(BaseClearButton)`
   padding: ${props => props.theme.spacing.tiny};
 `;
